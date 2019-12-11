a75cd7aaf3490dd98f674832842acc574197e51c
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-11 12:42:34 +0530

    Addad : daily limit for win or loose

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -2,17 +2,38 @@
 
 echo "Welcome : To Gambler Simulator "
 
-STAKE=100
+#constant
 BET=1
-WINS=0
 
-result=$((RANDOM%2))
-if [[ $result -eq 1 ]]
-then
-	echo "Win"
-	WINS=$(($WINS + 1))
-else
-	echo "loose"
-	WINS=$(($WINS - 0))
-fi
+#variable
+stack=100
+win=0
+loose=0
+lowerlimit=$(( $stack / 2 ))
+upperlimit=$(( $stack + $stack / 2 ))
 
+function dailyPlay()
+{
+while (( $lowerlimit < $stack && $upperlimit > $stack ))
+do
+	result=$((RANDOM%2))
+	if [[ $result -eq 1 ]]
+	then
+		((stack++))
+		((win++))
+	else
+		((stack--))
+		((loose++))
+	fi
+done
+echo $stack
+echo $win
+echo $loose
+}
+
+
+main()
+{
+  dailyPlay
+}
+main
