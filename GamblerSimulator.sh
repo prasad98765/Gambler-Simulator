35d110291d5070e36ac90f3e302c15cf680cde3b
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-11 11:44:30 +0530

    Added : Display Win or Loose using Random Function

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -4,3 +4,15 @@ echo "Welcome : To Gambler Simulator "
 
 STAKE=100
 BET=1
+WINS=0
+
+result=$((RANDOM%2))
+if [[ $result -eq 1 ]]
+then
+	echo "Win"
+	WINS=$(($WINS + 1))
+else
+	echo "loose"
+	WINS=$(($WINS - 0))
+fi
+
